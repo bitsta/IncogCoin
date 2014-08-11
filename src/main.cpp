6adc35c5b2a1779a4223952cbb10506c7f426774
bitsta <bitsta@continuumtalk.com>
2014-08-11 02:03:02 +0200

    added GetProofOfStakeReward_V2()

--- src/main.cpp
+++ src/main.cpp
@@ -990,6 +990,36 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
     return nSubsidy + nFees;
 }
 
+int64_t GetProofOfStakeReward_V1(int64_t nCoinAge, int64_t nFees)
+{
+    int64_t nRewardCoinYear;
+
+    nRewardCoinYear = MAX_MINT_PROOF_OF_STAKE;
+
+    int64_t nSubsidy = nCoinAge * nRewardCoinYear / 365 / COIN;
+
+
+    if (fDebug && GetBoolArg("-printcreation"))
+        printf("GetProofOfStakeReward(): create=%s nCoinAge=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nCoinAge);
+
+    return nSubsidy + nFees;
+}
+
+int64_t GetProofOfStakeReward_V2(int64_t nCoinAge, int64_t nFees)
+{
+    int64_t nRewardCoinYear;
+
+    nRewardCoinYear = MAX_MINT_PROOF_OF_STAKEV2;
+
+    int64_t nSubsidy = nCoinAge * nRewardCoinYear / 365 / COIN;
+
+
+    if (fDebug && GetBoolArg("-printcreation"))
+        printf("GetProofOfStakeReward(): create=%s nCoinAge=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nCoinAge);
+
+    return nSubsidy + nFees;
+}
+
 static const int64_t nTargetTimespan = 3 * 30;  // 3 blocks
 //
 // maximum nBits value could possible be required nTime after
